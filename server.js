66745aeaf90eaa151da413776f1b26744f2f5cc5
lasindu-themiya <lasinduthemiya96@gmail.com>
2026-09-10 14:37:56 +0530

    updated contact form

--- server.js
+++ server.js
@@ -197,6 +197,10 @@ app.get('/api/test-email', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Email server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Email server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
